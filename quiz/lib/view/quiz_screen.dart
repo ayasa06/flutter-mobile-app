d006e57f9c16cebfa38ff5720cdfc95431aa8afc
ayasa06 <111953500+ayasa06@users.noreply.github.com>
2024-07-11 10:46:34 +0900

    quiz_screen.dart更新

--- quiz/lib/view/quiz_screen.dart
+++ quiz/lib/view/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../database/database_helper.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
@@ -7,39 +8,71 @@ class QuizScreen extends StatefulWidget {
 
 class _QuizScreenState extends State<QuizScreen> {
   int questionNumber = 1;
-  String mushroomName = 'アカハツ'; // Replace with actual data
-  String mushroomImage =
-      'assets/mushrooms/tukiyotake.png'; // Replace with actual data
+  late List<Map<String, dynamic>> _quizData;
+  bool _isLoading = true;
 
-  void _answer(bool isEdible) {
-    Navigator.pushNamed(
-      context,
-      '/explanation',
-      arguments: {
-        'isEdible': isEdible,
-        'mushroomName': mushroomName,
-        'mushroomImage': mushroomImage,
-      },
-    );
+  @override
+  void initState() {
+    super.initState();
+    _fetchQuizData();
+  }
+
+  Future<void> _fetchQuizData() async {
+    var db = DatabaseHelper.instance;
+    _quizData = await db.queryRowsByDifficulty('easy');
+    print('Fetched quiz data count: ${_quizData.length}');
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Mushroom Quiz'),
+          backgroundColor: Colors.orange,
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    if (_quizData.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Mushroom Quiz'),
+          backgroundColor: Colors.orange,
+        ),
+        body: Center(
+          child: Text('クイズデータが存在しません。'),
+        ),
+      );
+    }
+
+    Map<String, dynamic> currentQuestion = _quizData[questionNumber - 1];
+    String questionText = currentQuestion[DatabaseHelper.columnQuestion];
+    String correctAnswer = currentQuestion[DatabaseHelper.columnAnswer];
+    String mushroomName = currentQuestion[DatabaseHelper.columnMushroomName];
+    String mushroomImage = currentQuestion[DatabaseHelper.columnMushroomImage];
+    List<String> options = currentQuestion[DatabaseHelper.columnOptions].split(',');
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Quiz'),
+        title: Text('Mushroom Quiz'),
         backgroundColor: Colors.orange,
       ),
       body: Padding(
-        padding: const EdgeInsets.only(
-            top: 40.0, left: 20.0, right: 20.0), // Added top padding
+        padding: const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, // Align to the top
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
-                '$questionNumber/10',
+                '$questionNumber/5',
                 style: TextStyle(fontSize: 24),
               ),
             ),
@@ -50,7 +83,7 @@ class _QuizScreenState extends State<QuizScreen> {
                 style: TextStyle(fontSize: 24),
               ),
             ),
-            SizedBox(height: 10.0), // Reduced space between text and image
+            SizedBox(height: 10.0),
             Image.asset(
               mushroomImage,
               height: 200.0,
@@ -58,36 +91,22 @@ class _QuizScreenState extends State<QuizScreen> {
             SizedBox(height: 60.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _answer(false),
+              children: options.map((option) {
+                return ElevatedButton(
+                  onPressed: () {
+                    _answer(option == correctAnswer, mushroomName, mushroomImage);
+                  },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFFFF4F50),
-                    padding:
-                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                    backgroundColor: option == 'はい' ? Color(0xFFFF4F50) : Color(0xFF4169E1),
+                    padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
                     textStyle: TextStyle(fontSize: 20),
                     shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(8.0), // More rectangular shape
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
-                  child: Text('有毒'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _answer(true),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF4169E1),
-                    padding:
-                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
-                    textStyle: TextStyle(fontSize: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(8.0), // More rectangular shape
-                    ),
-                  ),
-                  child: Text('食用'),
-                ),
-              ],
+                  child: Text(option),
+                );
+              }).toList(),
             ),
           ],
         ),
@@ -97,5 +116,25 @@ class _QuizScreenState extends State<QuizScreen> {
         color: Colors.orange,
       ),
     );
+  }
+
+  void _answer(bool isCorrect, String mushroomName, String mushroomImage) async {
+    bool? shouldContinue = await Navigator.pushNamed(
+      context,
+      '/explanation',
+      arguments: {
+        'isEdible': isCorrect,
+        'mushroomName': mushroomName,
+        'mushroomImage': mushroomImage,
+      },
+    ) as bool?;
+
+    if (shouldContinue == true && questionNumber < 5) {
+      setState(() {
+        questionNumber++;
+      });
+    } else if (shouldContinue == true) {
+      Navigator.pop(context);
+    }
   }
 }
