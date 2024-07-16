c1648a8b1df09c3cfb75556596d85bd1c6dccc4c
ayasa06 <111953500+ayasa06@users.noreply.github.com>
2024-07-17 02:31:35 +0900

    main.dart編集

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -5,59 +5,59 @@ import 'view/quiz_screen.dart';
 import 'view/explanation_screen.dart';
 import 'view/mushroom_catalog_screen.dart';
 import 'view/mushroom_detail_screen.dart';
-import 'database/database_helper.dart';
+import 'view/result_screen.dart'; 
+import 'database/database_helper.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   var db = DatabaseHelper.instance;
 
-  // 初期データを挿入
   var quizData = await db.queryAllRows();
   print('Initial data count: ${quizData.length}');
 
   if (quizData.isEmpty) {
     List<Map<String, dynamic>> initialData = [
       {
-        DatabaseHelper.columnQuestion: 'このキノコは有毒ですか？',
-        DatabaseHelper.columnAnswer: 'はい',
-        DatabaseHelper.columnOptions: 'はい,いいえ',
+        DatabaseHelper.columnQuestion: '',
+        DatabaseHelper.columnAnswer: '有毒',
+        DatabaseHelper.columnOptions: '有毒,食用',
         DatabaseHelper.columnDifficulty: 'easy',
         DatabaseHelper.columnMushroomName: 'ツキヨタケ',
         DatabaseHelper.columnMushroomImage: 'assets/mushrooms/tukiyotake.png',
         DatabaseHelper.columnMushroomDescription: 'ツキヨタケは有毒なキノコの一つ。見た目がマツタケに似ているが、マツタケと違って赤い傘を持ち、白い斑点がある。'
       },
       {
-        DatabaseHelper.columnQuestion: 'このキノコは食用ですか？',
-        DatabaseHelper.columnAnswer: 'いいえ',
-        DatabaseHelper.columnOptions: 'はい,いいえ',
+        DatabaseHelper.columnQuestion: '',
+        DatabaseHelper.columnAnswer: '食用',
+        DatabaseHelper.columnOptions: '有毒,食用',
         DatabaseHelper.columnDifficulty: 'easy',
         DatabaseHelper.columnMushroomName: 'アミガサタケ',
         DatabaseHelper.columnMushroomImage: 'assets/mushrooms/amigasatake.jpg',
         DatabaseHelper.columnMushroomDescription: 'アミガサタケは食用のキノコで、フランス料理などで人気がある。しかし、食べる前に十分な加熱が必要。'
       },
       {
-        DatabaseHelper.columnQuestion: 'このキノコは毒キノコですか？',
-        DatabaseHelper.columnAnswer: 'はい',
-        DatabaseHelper.columnOptions: 'はい,いいえ',
+        DatabaseHelper.columnQuestion: '',
+        DatabaseHelper.columnAnswer: '有毒',
+        DatabaseHelper.columnOptions: '有毒,食用',
         DatabaseHelper.columnDifficulty: 'easy',
         DatabaseHelper.columnMushroomName: 'ドクツルタケ',
         DatabaseHelper.columnMushroomImage: 'assets/mushrooms/dokuturutake.jpg',
         DatabaseHelper.columnMushroomDescription: 'ドクツルタケは非常に毒性の強いキノコで、食べると命にかかわることがあります。'
       },
       {
-        DatabaseHelper.columnQuestion: 'このキノコは食べられますか？',
-        DatabaseHelper.columnAnswer: 'はい',
-        DatabaseHelper.columnOptions: 'はい,いいえ',
+        DatabaseHelper.columnQuestion: '',
+        DatabaseHelper.columnAnswer: '食用',
+        DatabaseHelper.columnOptions: '有毒,食用',
         DatabaseHelper.columnDifficulty: 'easy',
         DatabaseHelper.columnMushroomName: 'マツタケ',
         DatabaseHelper.columnMushroomImage: 'assets/mushrooms/matutake.jpg',
         DatabaseHelper.columnMushroomDescription: 'マツタケは日本料理で高級食材とされており、秋の味覚として知られています。'
       },
       {
-        DatabaseHelper.columnQuestion: 'このキノコは食べられますか？',
-        DatabaseHelper.columnAnswer: 'いいえ',
-        DatabaseHelper.columnOptions: 'はい,いいえ',
+        DatabaseHelper.columnQuestion: '',
+        DatabaseHelper.columnAnswer: '有毒',
+        DatabaseHelper.columnOptions: '有毒,食用',
         DatabaseHelper.columnDifficulty: 'easy',
         DatabaseHelper.columnMushroomName: 'カエンタケ',
         DatabaseHelper.columnMushroomImage: 'assets/mushrooms/kaentake.jpg',
@@ -68,7 +68,6 @@ void main() async {
       await db.insert(data);
     }
 
-    // データを挿入した後に再度クエリを実行して確認
     quizData = await db.queryAllRows();
     print('Data after insertion: ${quizData.length}');
   }
@@ -92,6 +91,7 @@ class MyApp extends StatelessWidget {
         '/explanation': (context) => ExplanationScreen(),
         '/mushroom_catalog': (context) => MushroomCatalogScreen(),
         '/mushroom_detail': (context) => MushroomDetailScreen(),
+        '/result': (context) => ResultScreen(correctAnswers: 0, quizData: []), // デフォルト値を設定
       },
     );
   }
